93faf67442fb6a6123869ec580a6c00052bc7ba6
Dhyey <dhyeykakadiya1015@gmail.com>
2024-08-30 18:42:26 +0530

    fixed an error which was checking only last two added information

--- mombuddy/chatbot/views.py
+++ mombuddy/chatbot/views.py
@@ -6,7 +6,7 @@ from .models import FAQ
 def chatbot_view(request):
     response = None
     if request.method == 'POST':
-        user_query = request.POST.get('query', '')
+        user_query = request.POST.get('query', '') #('query': "whar is django")
         keywords = user_query.lower().split()
         
         faqs = FAQ.objects.all()
@@ -18,13 +18,38 @@ def chatbot_view(request):
             keyword_matches = sum(1 for keyword in keywords if keyword in faq_keywords)#<=========== don't know
             
             if keyword_matches > max_keywords_matches:
-                max_keyword_matches = keyword_matches
+                max_keywords_matches = keyword_matches
                 matched_faq = faq
-        if matched_faq and max_keyword_matches > 0:
+        if matched_faq and max_keywords_matches > 0:
             response = matched_faq.answer
                 
-        else:
+        elif (response):
             response = "I'm sorry, I don't have an answer for that."
             
     return render(request, 'chatbot/chat.html', {'response': response})
     
+    
+    
+ # my logic   
+# from django.shortcuts import render
+# from .models import FAQ
+
+# def chatbot_view(request):
+#     response = None
+#     if request.method == 'POST':
+#         user_query = request.POST.get('query', '')
+#         keywords = user_query.lower().split()
+        
+#         faqs = FAQ.objects.all()
+#         for faq in faqs:
+#             for keyword in keywords:
+#                 if keyword in faq.question.lower():
+#                     response = faq.answer
+#                     break
+#             if response:
+#                 break
+#         if not response:
+#             response = "I'm sorry, I don't have an answer for that."
+    
+#     return render(request, 'chatbot/chat.html', {'response': response})
+
